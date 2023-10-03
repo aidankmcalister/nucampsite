96bf3afd0852342c58df392d77aa5421831f3650
Aidan <aidankmcalister@gmail.com>
2023-10-03 15:50:39 -0500

    Weather info in nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 console.log("javascript connected!");
+const city = "Tulsa";
+const apiKey = process.env.OPEN_WEATHER_API_KEY;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 const carousel = new bootstrap.Carousel("#homeCarousel", {
   interval: 2000,
@@ -19,3 +22,33 @@ carouselButton.addEventListener("click", function () {
     carousel.cycle();
   }
 });
+
+async function fetchWeather() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    displayWeather(data);
+  } catch (error) {
+    console.error("There has been an error", error);
+  }
+}
+
+fetchWeather();
+
+function displayWeather(data) {
+  const temperature = data.main.temp;
+  const description = data.weather[0].description;
+  const icon = data.weather[0].icon;
+
+  console.log(temperature, icon, description);
+
+  const iconURL = `https://openweathermap.org/img/w/${icon}.png`;
+  const iconImg = document.createElement("img");
+  iconImg.src = iconURL;
+  document.querySelector("#weather-icon").appendChild(iconImg);
+
+  document.querySelector("#weather-temp").textContent = `${temperature} \u00B0`;
+
+  document.querySelector("#weather-description").textContent = description;
+}
